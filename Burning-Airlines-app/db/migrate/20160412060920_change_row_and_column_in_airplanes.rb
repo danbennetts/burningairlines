3a5540d9ef6034d7252053027db5acd8234f02ea
twosmalltrees <harrison@twosmalltrees.com>
2016-04-12 16:18:28 +1000

    Changing airplane column names and data types

---
+++ Burning-Airlines-app/db/migrate/20160412060920_change_row_and_column_in_airplanes.rb
@@ -0,0 +1,6 @@
+class ChangeRowAndColumnInAirplanes < ActiveRecord::Migration
+  def change
+    rename_column :airplanes, :row, :rows
+    rename_column :airplanes, :column, :columns
+  end
+end
--- Burning-Airlines-app/db/schema.rb
+++ Burning-Airlines-app/db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412054217) do
+ActiveRecord::Schema.define(version: 20160412061557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "airplanes", force: :cascade do |t|
     t.string   "name"
-    t.integer  "row"
-    t.string   "column"
+    t.integer  "rows"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "columns"
   end
 
   create_table "flights", force: :cascade do |t|
