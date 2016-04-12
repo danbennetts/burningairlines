bffff1c7e99736e0fa2afeeeed56bab31f4210cb
twosmalltrees <harrison@twosmalltrees.com>
2016-04-12 23:13:57 +1000

    Edit Flight Model defaults

--- Burning-Airlines-app/app/assets/javascripts/models/Flight.js
+++ Burning-Airlines-app/app/assets/javascripts/models/Flight.js
@@ -3,9 +3,8 @@ var app = app || {};
 app.Flight = Backbone.Model.extend({
   urlRoot: '/flights',
   defaults: {
-    // Possibly more defaults required...maybe
-    flight_number: nil,
+    flight_number: 0,
     origin: 'unknown',
-    destination: 'unknown',
+    destination: 'unknown'
   }
 });
