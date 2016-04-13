dac32ec967ca16e216b0342d28a8c13d6f785379
twosmalltrees <harrison@twosmalltrees.com>
2016-04-13 11:34:42 +1000

    Editing reservation view list

--- Burning-Airlines-app/app/assets/javascripts/views/ReservationView.js
+++ Burning-Airlines-app/app/assets/javascripts/views/ReservationView.js
@@ -5,22 +5,37 @@ app.ReservationView = Backbone.View.extend({
   tagName: 'ul',
 
   render: function() {
-    var reservationID = this.model.get("id");
-    reservation_id_li = $('<li>');
-    reservation_id_li.text(reservationID);
-    this.$el.append(reservation_id_li);
 
-    var userID = this.model.get("user_id");
-    var flightID = this.model.get("flight_id");
+    var flightNum = this.model.get("flight_number");
+    var $flightNumLi = $('<li>');
+    $flightNumLi.text(flightNum);
+
+    var origin = this.model.get("origin");
+    var $originLi = $('<li>');
+    $originLi.text(origin);
+
+    var destination = this.model.get("destination");
+    var $destinationLi = $('<li>');
+    $destinationLi.text(destination);
+
+    var date = this.model.get("date");
+    var $dateLi = $('<li>');
+    $dateLi.text(date);
+
     var seatRow = this.model.get("seat_row");
     var seatColumn = this.model.get("seat_column");
-    var flightNum = this.model.get("flight_number");
-    var origin = this.model.get("origin");
-    var destination = this.model.get("destination");
-    var date = this.model.get("date");
+    var $seatInfoLi = $('<li>');
+    $seatInfoLi.text(seatRow + seatColumn);
+
+
+
+    var userID = this.model.get("user_id");
+    var flightID = this.model.get("flight_id"); // Need that
     var airplaneID = this.model.get("airplane_id");
     var planeName = this.model.get("name");
+    var reservationID = this.model.get("id");
 
+    this.$el.append($flightNumLi, $originLi, $destinationLi, $dateLi, $seatInfoLi);
     this.$el.prependTo('#reservations');
   }
 
