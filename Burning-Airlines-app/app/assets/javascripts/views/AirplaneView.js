c53b2aff8812b27f9e9a21d977012e9726f92607
cjrcooper <Coops@Christophers-MacBook-Pro-2.local>
2016-04-14 00:38:12 +1000

    added in the plane view and the seating arrangement

--- Burning-Airlines-app/app/assets/javascripts/views/AirplaneView.js
+++ Burning-Airlines-app/app/assets/javascripts/views/AirplaneView.js
@@ -10,6 +10,28 @@ app.AirplaneView = Backbone.View.extend({
     var airplaneViewHTML = _.template( airplaneViewTemplate );
     this.$el.html( airplaneViewHTML( this.model.toJSON() ) );
     this.$el.prependTo('#reservations');
+    this.createRow();
+    this.createSeats();
+    this.setSets();
+  },
 
+  createRow: function(){
+    for (var i = 1; i <= this.model.get('rows'); i++){
+      $("#main").append('<div class="row" id=' + i + '></div>');
+    };
+  },
+
+  createSeats: function(){
+    var alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    for (var i = 0; i < this.model.get('columns'); i++){
+      $('.row').append('<div class="seat"  data-seat=' + alpha[i] + '></div>');
+    }
+  },
+
+  setSets: function(){
+    $.each($('.seat'), function (i, seat) {
+      var setSeats = $(seat).text($(this).data('seat'));
+    });
   }
+
 });
