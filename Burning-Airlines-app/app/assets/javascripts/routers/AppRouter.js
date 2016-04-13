23a164deb480abe849db051de70d5df654edbe0e
Daniel Bennetts <daniel.bennetts@bigpond.com>
2016-04-13 11:53:29 +1000

    merged

--- Burning-Airlines-app/app/assets/javascripts/routers/AppRouter.js
+++ Burning-Airlines-app/app/assets/javascripts/routers/AppRouter.js
@@ -3,9 +3,15 @@ var app = app || {};
 app.AppRouter = Backbone.Router.extend({
   routes: {
     '': 'index',
+    'search': 'searchShow'
   },
   index: function() {
     var appView = new app.AppView();
     appView.render();
+  },
+  searchShow: function() {
+    $('#main').html('');
+    var searchView = new app.SearchView({collection: flights});
+    searchView.render();
   }
 });
