b83b34033413730d8c2b586061840eb33a7d41d3
Daniel Bennetts <daniel.bennetts@bigpond.com>
2016-04-13 11:13:28 +1000

    added data from json for view

--- Burning-Airlines-app/app/assets/javascripts/views/ReservationView.js
+++ Burning-Airlines-app/app/assets/javascripts/views/ReservationView.js
@@ -2,10 +2,20 @@ var app = app || {};
 
 app.ReservationView = Backbone.View.extend({
 
-  tagName: 'li',
+  tagName: 'ul',
 
   render: function() {
-
+    var reservationID = this.model.get("id");
+    var userID = this.model.get("user_id");
+    var flightID = this.model.get("flight_id");
+    var seatRow = this.model.get("seat_row");
+    var seatColumn = this.model.get("seat_column");
+    var flightNum = this.model.get("flight_number");
+    var origin = this.model.get("origin");
+    var destination = this.model.get("destination");
+    var date = this.model.get("date");
+    var airplaneID = this.model.get("airplane_id")
+    var planeName = this.model.get("name");
   }
 
 });
