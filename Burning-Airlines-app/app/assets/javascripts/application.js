1314cb68132b6a00e75e4a8eb47628b154b04565
Daniel Bennetts <daniel.bennetts@bigpond.com>
2016-04-14 14:28:26 +1000

    added bootstrap and fontawesome

--- Burning-Airlines-app/app/assets/javascripts/application.js
+++ Burning-Airlines-app/app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
+//= require 'fontawesome'
 //= require lib/underscore
 //= require lib/backbone
 //= require lib/backbone.localStorage-min
